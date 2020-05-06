9011c6c140e69ffa7af51d52a6c3e8e49cf0a7c3
Eric Montzka <emontzka@sbcglobal.net>
2020-05-06 10:10:21 -0500

    add filters

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
 import React, { useState, useEffect } from 'react';
-import logo from './images/logo.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 import './App.scss';
 import data from './data/productData.json';
 import Product from './components/product';
+import logo from './images/logo.png';
 
-const App = () => {
+function App() {
   const [products, setProducts] = useState(null);
+  const [menu, setMenu] = useState(false);
+  const toggleMenu = () => {
+    setMenu(!menu);
+  };
+
   useEffect(() => setProducts(data), []);
+
   if (products === null) {
     return 'Loading...';
   }
   return (
     <div className="App">
+      <div className={menu ? 'mobile-menu open' : 'mobile-menu'}>
+        <FontAwesomeIcon onClick={toggleMenu} icon={faTimes} size="lg" />
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Products</a></li>
+          <li><a href="#">Help</a></li>
+        </ul>
+      </div>
       <header className="App-header">
         <div className="container-fluid">
           <div className="row">
@@ -24,6 +41,7 @@ const App = () => {
                   <li><a href="#">Products</a></li>
                   <li><a href="#">Help</a></li>
                 </ul>
+                <FontAwesomeIcon onClick={toggleMenu} icon={faBars} size="lg" />
               </nav>
               <h1 className="h1 page-title">This is a headline</h1>
             </div>
@@ -35,6 +53,18 @@ const App = () => {
         <div className="container-fluid">
           <div className="row">
             <div className="col-xl-3 col-md-4">
+              <div className="row">
+                <div className="col-12">
+                  <ul className="filters">
+                    <li>Footwear</li>
+                    <li>Water Fountains</li>
+                    <li>Garage Door Openers</li>
+                    <li>Smart Home Tech</li>
+                    <li>Beverages</li>
+                    <li>Education</li>
+                  </ul>
+                </div>
+              </div>
 
             </div>
             <div className="col-xl-9 col-md-8">
